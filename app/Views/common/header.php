3afaa0d9dad9eb0b2da3b956866649d8e1d386a1
Akash Jaisawal <aj9125747@gmail.com>
2026-01-09 18:05:51 +0530

    created some popup design

--- app/Views/common/header.php
+++ app/Views/common/header.php
@@ -5,6 +5,8 @@
 <head>
     <meta charset="UTF-8" />
     <title>CA Project</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
