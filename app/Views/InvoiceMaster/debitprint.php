cba5716dd167eb713377145c1da68761696b9615
sapple-akashjaiswal <akash.jaiswal@sapple.co.in>
2026-06-09 16:46:46 +0530

    work on print debit

--- app/Views/InvoiceMaster/debitprint.php
+++ app/Views/InvoiceMaster/debitprint.php
@@ -1,12 +1,23 @@
 <style>
+    body{
+    font-family: 'Cambria', serif!important;
+}
+     @font-face {
+    font-family: 'Cambria';
+    src: url("<?= base_url('public/fonts/Cambria.ttf') ?>") format('truetype'),
+         url("<?= base_url('public/fonts/Cambria.woff') ?>") format('woff'),
+         url("<?= base_url('public/fonts/Cambria.woff2') ?>") format('woff2');
+    font-weight: normal;
+    font-style: normal;
+}
 .debitnotepdf-invoice {
     width: 100%;
     max-width: 750px;
     box-sizing: border-box;
     margin: 20px auto;
-    border: 1px solid #000;
-    padding: 15px;
-    font-family: Arial, sans-serif;
+    border: 1px solid #D3D3D3;
+    padding: 10px;
+    font-family: 'Cambria', serif !important;
     background: #fff;
 }
 
@@ -19,6 +30,7 @@
     display: table-cell;
     width: 75%;
     vertical-align: top;
+    font-size:14px;
 }
 
 .debitnotepdf-logo {
@@ -62,7 +74,7 @@
 .debitnotepdf-table,
 .debitnotepdf-table th,
 .debitnotepdf-table td {
-    border: 1px solid #000;
+    border: 1px solid #BDBDBD;
 }
 
 .debitnotepdf-table th,
@@ -72,7 +84,7 @@
 }
 
 .debitnotepdf-table th {
-    background: #eee;
+    background: #F5F5F5;
 }
 
 .debitnotepdf-text-left {
@@ -98,6 +110,49 @@
     font-size: 12px;
     margin-top: 15px;
 }
+.debitnotepdf-bank{
+    width: 100%;
+    border: 1px solid #BDBDBD;
+    display: table;
+    table-layout: fixed;
+    min-height: 170px;
+}
+
+.debitnotepdf-bank-left{
+    width: 50%;
+    display: table-cell;
+    vertical-align: top;
+    padding: 10px;
+    border-right: 1px solid #BDBDBD;
+    line-height: 24px;
+}
+
+.debitnotepdf-bank-right{
+    width: 50%;
+    display: table-cell;
+    vertical-align: top;
+    position: relative;
+    padding: 10px;
+    height: 170px;
+}
+
+.debitnotepdf-company-name{
+    text-align: right;
+    font-size: 14px;
+    vertical-align: top;
+    font-weight:bold;
+}
+
+.debitnotepdf-sign{
+    position: absolute;
+    bottom: 0;
+    right: 10px;
+    font-size: 16px;
+    margin: 0;
+    padding: 0;
+    font-weight: bold;
+    
+}
 @media print {
 
     body {
@@ -110,7 +165,7 @@
         max-width: 100% !important;
         margin: 0 !important;
         padding: 10px !important;
-        border: 1px solid #000;
+        border: 1px solid #D3D3D3;
         box-sizing: border-box;
     }
 
@@ -137,13 +192,35 @@
 {
     margin-top:0px;
 }
+.debitnotepdf-info-row {
+    width: 100%;
+    display: table;
+    table-layout: fixed;
+}
+
+.debitnotepdf-left {
+    display: table-cell;
+    width: 70%;
+    vertical-align: top;
+}
+
+.debitnotepdf-right {
+    display: table-cell;
+    width: 30%;
+    vertical-align: top;
+    text-align: left;
+    padding-left: 40px;
+}
 </style>
 <div class="debitnotepdf-invoice">
 
     <div class="debitnotepdf-header ">
         <div class="debitnotepdf-company-details">
-            <h3 class="debitnotepdf-company-name"><?php echo $company['name']; ?></h3>
-            <b><?php echo $company['type_of_company']; ?></b><br />
+            <strong style="font-size:22px;"><?php echo $company['name']; ?></strong><br>
+            <span style="font-weight:600;font-size:15px">
+                            <?php echo $company['type_of_company']; ?>
+                        </span><br>
+        
             Address : <?php echo $company['registered_office']; ?><br />
             Ph. No. : <?php echo $company['telephone']; ?><br />
             E-mail : <?php echo $company['email']; ?><br />
@@ -152,7 +229,7 @@
         <div class="debitnotepdf-logo">
            <img src="<?= base_url('public/uploads/company_logo/' . $company['logo']); ?>" 
 alt="Company Logo"
-style="width:120px; height:auto; display:block; margin-left:auto;">
+style="width:180px; height:auto; display:block; margin-left:auto;">
         </div>
     </div>
 
@@ -164,24 +241,26 @@ style="width:120px; height:auto; display:block; margin-left:auto;">
 
     <div class="debitnotepdf-info">
         <div class="debitnotepdf-info-row">
-            
-            <div>
-                <?php if ($debitNote['note_type'] === 'debit') : ?>
-                <b>Invoice No.:</b> <?= esc($debitNote['debit_no']); ?><br />
-                <?php elseif ($debitNote['note_type'] === 'credit') : ?>
-                <b>Invoice No.:</b> <?= esc($debitNote['credit_no']); ?><br />
-                <?php endif; ?>
-                <b>Date:</b> <?php echo $debitNote['date']; ?>
-            </div>
-        </div>
 
-        <br />
+    <div class="debitnotepdf-left">
         <b>Issued To,</b><br />
-        <b>Name :</b><?php echo $client['legal_name']; ?> <br />
-        <b>Address :</b><?php echo $client['registered_office']; ?> <br />
-        <b>Email :</b><?php echo $client['email']; ?> <br />
-        <b>GSTIN :</b><?php echo $client['gstin']; ?> <br />
+        <b>Name :</b> <?php echo $client['legal_name']; ?> <br />
+        <b>Address :</b> <?php echo $client['registered_office']; ?> <br />
+        <b>Email :</b> <?php echo $client['email']; ?> <br />
+        <b>GSTIN :</b> <?php echo $client['gstin']; ?> <br />
     </div>
+
+    <div class="debitnotepdf-right">
+        <?php if ($debitNote['note_type'] === 'debit') : ?>
+        <b>Invoice No.:</b> <?= esc($debitNote['debit_no']); ?><br />
+        <?php elseif ($debitNote['note_type'] === 'credit') : ?>
+        <b>Invoice No.:</b> <?= esc($debitNote['credit_no']); ?><br />
+        <?php endif; ?>
+
+        <b>Date:</b> <?php echo $debitNote['date']; ?>
+    </div>
+
+</div>
 
     <p class="debitnotepdf-note">
         Kindly acknowledge the expenses incurred by us on your behalf, given
@@ -234,24 +313,35 @@ style="width:120px; height:auto; display:block; margin-left:auto;">
     </table>
 
 
-    <div class="debitnotepdf-bank">
-        <div>
-            <b>Banker's Details</b><br />
-            <?= esc($company['name']); ?></br>
-          Bank Name:<?php echo $company['bank_name']; ?><br />
-          A/C.No. : <?php echo $company['bank_ac_no']; ?><br />
-          IFSC Code : <?php echo $company['bank_ifsc']; ?><br />
-          Branch :<?php echo $company['branch_address']; ?>
-        </div>
-        <div class="debitnotepdf-sign">
-            <b><?php echo $company['name']; ?></b><br /><br /><br />
-            Authorised Signatory
-        </div>
+  <div class="debitnotepdf-bank">
+
+    <div class="debitnotepdf-bank-left">
+        <b>Banker's Details</b><br>
+
+        <?= esc($company['name']); ?><br>
+
+        Bank Name : <?php echo $company['bank_name']; ?><br>
+
+        A/C.No. : <?php echo $company['bank_ac_no']; ?><br>
+
+        IFSC Code : <?php echo $company['bank_ifsc']; ?><br>
+
+        Branch : <?php echo $company['branch_address']; ?>
     </div>
 
-    <!-- <div class="right">
-        <button onclick="window.print()">Print Invoice</button>
-    </div> -->
+    <div class="debitnotepdf-bank-right">
+
+        <div class="debitnotepdf-company-name">
+          <?php echo $company['name']; ?>
+        </div>
+
+        <div class="debitnotepdf-sign">
+            Authorised Signatory
+        </div>
+
+    </div>
+
+</div>
 </div>
 <script>
 window.onload = function() {
