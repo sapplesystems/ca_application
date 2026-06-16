b2881933ab240dd641244770f138c9a5e8544f5e
sapple-akashjaiswal <akash.jaiswal@sapple.co.in>
2026-06-16 11:03:11 +0530

    work on cancel button which is not working

--- app/Views/InvoiceMaster/debitprint.php
+++ app/Views/InvoiceMaster/debitprint.php
@@ -344,7 +344,11 @@ style="width:180px; height:auto; display:block; margin-left:auto;">
 </div>
 </div>
 <script>
-window.onload = function() {
+window.onload = function () {
     window.print();
+};
+
+window.onafterprint = function () {
+    window.location.href = "<?= base_url('invoice-mangement'); ?>";
 };
 </script>
