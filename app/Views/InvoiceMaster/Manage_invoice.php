5efc0176cc1cb991bc8ed355825cf5f31c531a3b
sapplesystems <business@sapple.co.in>
2026-06-30 12:44:36 +0530

    work on the print ledeger

--- app/Views/InvoiceMaster/Manage_invoice.php
+++ app/Views/InvoiceMaster/Manage_invoice.php
@@ -1074,32 +1074,14 @@ foreach ($receipt as $rec) {
             <link rel="stylesheet" href="assets/css/style.css">
            <style>
           
-          @page {
-            size: A4 portrait;
-            margin: 10mm;
-            }
+    body { font-family: Cambria, sans-serif; }
 
-            html,
-            body {
-                width: 210mm;
-                min-height: 297mm;
-                margin: 0;
-                padding: 0;
-                font-family: Cambria, sans-serif;
-            }
-            body { font-family: Cambria, sans-serif; }
+    table { width: 100%; border-collapse: collapse; }
 
-            table { width: 100%; border-collapse: collapse; }
+    th, td { border: 1px solid #000; padding: 6px; }
 
-            th, td { border: 1px solid #000; padding: 6px; }
+    .Minvoice-text-right { text-align: right; }
 
-            .Minvoice-text-right { text-align: right; }
-
-            th,td {
-                word-wrap: break-word;
-                overflow-wrap: break-word;
-                white-space: normal;
-            }
   
 
     @media print {
